945d43f7e5e12a06010636e6aa9cec3921e6ab01
lindo101 <sibandalindo.him@gmail.com>
2023-03-22 04:05:02 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("\nSize of an int: %lu byte(s)", sizeof(int));
 	printf("\nSize of a long int: %lu byte(s)", sizeof(long int));
 	printf("\nSize of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("\nSize of a float: %lu byte(s)", sizeof(float));
+	printf("\nSize of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
